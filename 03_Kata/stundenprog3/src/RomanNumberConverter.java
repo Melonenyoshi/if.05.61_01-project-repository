231441812c0c6407b358af5c8a349793d41eeacc
Elias Kramer <67228319+EliasKramer@users.noreply.github.com>
2021-11-13 19:56:47 +0100

    third

--- 03_Kata/stundenprog3/src/RomanNumberConverter.java
+++ 03_Kata/stundenprog3/src/RomanNumberConverter.java
@@ -16,7 +16,8 @@ public class RomanNumberConverter {
     public static int toArabic(String romanNumber) {
         if(romanNumber.equals("II"))
             return 2;
-
+        if(romanNumber.equals("III"))
+            return 3;
         return 1;
     }
 
