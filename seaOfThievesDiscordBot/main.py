6da74e55cece42741f2ae3adb36b677944383c5c
T41PAN <58813092+T41PAN@users.noreply.github.com>
2022-01-28 11:23:18 +0100

    Hello World!

--- seaOfThievesDiscordBot/main.py
+++ seaOfThievesDiscordBot/main.py
@@ -2,17 +2,25 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = commands.Bot(command_prefix=".")
 
 
 @client.event
 async def on_ready():
+    activity = discord.Game(name="Sea of Thieves", type=1)
+    await client.change_presence(status=discord.Status.idle, activity=activity)
     print(f'{client.user} has connected to Discord!')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
 
 client.run(TOKEN)
