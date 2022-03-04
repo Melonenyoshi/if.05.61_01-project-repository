1f057742e3e13957139a9dd509e16e5018c25ebb
Elias Kramer <67228319+EliasKramer@users.noreply.github.com>
2022-03-04 08:55:57 +0100

    first line and image can now be given by the bot

--- seaOfThievesDiscordBot/main.py
+++ seaOfThievesDiscordBot/main.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
-
+from bs4 import BeautifulSoup
+import requests
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -22,5 +23,40 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
+@client.command()
+async def add(ctx, first_numb, second_numb):
+    first = 0
+    second = 0
+    try:
+        first = int(first_numb)
+        second = int(second_numb)
+    except:
+        await ctx.send("That aint even numbers")
+    else:
+        await ctx.send(f"the result is {first + second}")
+@client.command()
+async def web(ctx,given_search_string):
+    output = ""
+
+    path = "https://seaofthieves.fandom.com/wiki/Special:Search?query="+given_search_string
+
+    request = requests.get(path)
+
+    soup = BeautifulSoup(request.text, 'html.parser')
+
+    output += soup.title.string
+
+    image_div = soup.find("div", {"class": "mw-parser-output"})
+    children_of_image_div = image_div.findChildren("a", {"class": "image"}, recursive=True)
+    output += "\n" + children_of_image_div[0]["href"]
+
+    first_line_div = image_div;
+    children_of_line_div = first_line_div.findChildren("p")
+
+    output += "\n" + children_of_line_div[0].text;
+
+    print("output: \n"+output)
+
+    await ctx.send(output)
 
 client.run(TOKEN)
